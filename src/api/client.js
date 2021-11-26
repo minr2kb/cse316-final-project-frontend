665911214037332ddaf485d6676382832e1a096f
Kyungbae Min <kbmin1129@gmail.com>
2021-11-27 04:47:27 +0900

    API methods in client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,9 +1,117 @@
 // profile image input
-export const uploadImageToCloudinaryAPIMethod = formData => {
-	const cloudName = "";
+const SERVER_URL = "http://localhost:5001";
+
+const defaultHeaders = {
+	headers: {
+		"Content-Type": "application/json; charset=UTF-8",
+	},
+};
+
+export const uploadImageToCloudinaryAPI = formData => {
+	const cloudName = "ddrzspgjy";
 	return fetch(`https://api.cloudinary.com/v1_1/${cloudName}/upload`, {
 		method: "POST",
 		body: formData,
+	})
+		.then(checkStatus)
+		.then(parseJSON);
+};
+
+// User auth APIs
+
+export const getUserAPI = () => {
+	return fetch(`${SERVER_URL}/api/user`, {
+		...defaultHeaders,
+	}).then(response => {
+		if (response.status >= 500) {
+			return null;
+		} else {
+			return parseJSON(response);
+		}
+	});
+};
+
+export const updateUserAPI = user => {
+	return fetch(`${SERVER_URL}/api/user`, {
+		...defaultHeaders,
+		method: "PUT",
+		body: JSON.stringify(user),
+	}).then(checkStatus);
+};
+
+export const registerAPI = (name, email, password) => {
+	return fetch(`${SERVER_URL}/api/register`, {
+		...defaultHeaders,
+		method: "POST",
+		body: JSON.stringify({
+			userName: userName,
+			email: email,
+			password: password,
+		}),
+	}).then(response => {
+		if (response.status >= 500) {
+			return "duplicated";
+		}
+		return "success";
+	});
+};
+
+export const loginAPI = (email, password) => {
+	return fetch(`${SERVER_URL}/api/login`, {
+		...defaultHeaders,
+		method: "POST",
+		body: JSON.stringify({
+			email: email,
+			password: password,
+		}),
+	}).then(response => {
+		if (response.status < 400) {
+			return "success";
+		} else {
+			return "failed";
+		}
+	});
+};
+
+export const logoutAPI = () => {
+	return fetch(`${SERVER_URL}/api/logout`, {
+		...defaultHeaders,
+		method: "POST",
+		body: JSON.stringify([]),
+	}).then(checkStatus);
+};
+
+// questions APIs
+export const getQuestionsAPI = () => {
+	return fetch(`${SERVER_URL}/api/questions`, {
+		...defaultHeaders,
+	})
+		.then(checkStatus)
+		.then(parseJSON);
+};
+
+export const createQuestionAPI = question => {
+	return fetch(`/api/questions`, {
+		...defaultHeaders,
+		method: "POST",
+		body: JSON.stringify(question),
+	})
+		.then(checkStatus)
+		.then(parseJSON);
+};
+
+export const updateQuestionAPI = question => {
+	return fetch(`${SERVER_URL}/api/questions/${question._id}`, {
+		...defaultHeaders,
+		method: "PUT",
+		body: JSON.stringify(question),
+	}).then(checkStatus);
+};
+
+export const deleteQuestionByIdAPI = questionId => {
+	return fetch(`${SERVER_URL}/api/questions/${questionId}`, {
+		...defaultHeaders,
+		method: "DELETE",
 	})
 		.then(checkStatus)
 		.then(parseJSON);
