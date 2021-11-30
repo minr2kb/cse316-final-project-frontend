709790a04a83b3a63812530be1fbbef3485bb339
Biniam <84908082+Biniyoyo@users.noreply.github.com>
2021-11-30 22:20:21 +0900

    view css edited

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -88,28 +88,107 @@ function View(props) {
 	//temporary objects to catagorize questions
 
 	const dataForMultiple = [
+		// {
+		// 	name: " ",
+		// 	response1: 2,
+		// 	response2: 3,
+		// 	response3: 4,
+		// },
 		{
-			name: " ",
-			response1: 2,
-			response2: 3,
-			response3: 4,
+			_id: 3,
+			creator: "",
+			createdDate: "",
+			questionType: "multiple",
+			questionText: "What is your favorite color?",
+			multipleChoice: ["Red", "Green", "Blue"],
+			createdDate: new Date(),
+			responses: { "11/27/2021":0, "11/28/2021":1},	
 		},
+		{
+			_id: 3,
+			creator: "",
+			createdDate: "",
+			questionType: "multiple",
+			questionText: "Where India is located?",
+			multipleChoice: ["Asia", "N.America", "S.America"],
+			createdDate: new Date(),
+			responses: { "11/27/2021":0},
+		}
 	];
 
 	const dataForBool = [
-		{ name: 'True', value: 400 },
-		{ name: 'False', value: 400 },
+		// { name: 'True', value: 400 },
+		// { name: 'False', value: 400 },
+		{
+			_id: 2,
+			creator: "",
+			createdDate: "",
+			questionType: "boolean",
+			questionText: "Did you do your assignments?",
+			multipleChoice: [],
+			createdDate: new Date(),
+			responses: { "11/27/2021": true },
+		},
+		{
+			_id: 2,
+			creator: "",
+			createdDate: "",
+			questionType: "boolean",
+			questionText: "Water is liquid?",
+			multipleChoice: [],
+			createdDate: new Date(),
+			responses: { "11/28/2021": true },
+		}
 	];
 
 	const dataForNumber = [
+		// {
+		// 	name: "Question",
+		// 	response: [0,2],
+		// 	response2:2,
+		// },
 		{
-			name: "Question",
-			response: [0,2],
-			response2:2,
+			_id: 1,
+			creator: "",
+			createdDate: "",
+			questionType: "number",
+			questionText: "How old are you?",
+			multipleChoice: [],
+			createdDate: new Date(),
+			responses: { "11/27/2021": 22 },
 		},
+		{
+			_id: 1,
+			creator: "",
+			createdDate: "",
+			questionType: "number",
+			questionText: "What is your admission year?",
+			multipleChoice: [],
+			createdDate: new Date(),
+			responses: { "11/27/2021": 2019 },
+		}
 	];
 	const dataForText = [
-		
+		{
+			_id: 0,
+			creator: "",
+			createdDate: "",
+			questionType: "text",
+			questionText: "What is your major?",
+			multipleChoice: [],
+			createdDate: new Date(),
+			responses: { "11/30/2021": "Major: Ams & Minor: CSE" },
+		},
+		{
+			_id: 0,
+			creator: "",
+			createdDate: "",
+			questionType: "text",
+			questionText: "Korean food you like?",
+			multipleChoice: [],
+			createdDate: new Date(),
+			responses: { "11/29/2021": "Kimchi", "11/30/2021": "Kimbab", "11/31/2021": "Bibimbab"},
+		}
 	];
 
 	const COLORS = ['#0088FE', '#FF8042'];
@@ -137,17 +216,6 @@ function View(props) {
 					}
 				}
 				
-				const newQ = {
-					name: q.questionText,
-					response1: response1,
-					response2: response2,
-					response3: response3,
-				};
-
-				if(!dataForMultiple.includes(newQ)){
-					dataForMultiple.push(newQ);
-				};
-				// console.log(dataForMultiple);
 			}
 			if (q.questionType === "bool" && !dataForBool.includes(q)) {
 				dataForBool.push(q);
@@ -172,7 +240,7 @@ function View(props) {
 					<br />
 					{Object.keys(q.responses).map((d)=>(
 						<>
-						<div className="dateBlock">{d}</div>
+						<div className="dateBlock">{d}:</div>
 						<div className="responseBlock">{q.responses[d]}</div>
 						<br />
 						</>
@@ -183,76 +251,88 @@ function View(props) {
 			</div>
 
 			<div className="bar">
-				<p>What is your favorite color?</p>
-				<div width="100%" height="100%">
-					<BarChart
-						width={500}
-						height={300}
-						data={dataForMultiple}
-						margin={{
-							top: 5,
-							right: 30,
-							left: 20,
-							bottom: 5,
-						}}
-					>
-						<CartesianGrid strokeDasharray="3 3" />
-						<XAxis dataKey="name" />
-						<YAxis />
-						<Tooltip />
-						<Legend />
-						<Bar dataKey="response1" fill="#8884d8" />
-						<Bar dataKey="response2" fill="#82ca9d" />
-						<Bar dataKey="response3" fill="skyblue" />
-					</BarChart>
-				</div>
+				{dataForMultiple.map((q)=>(
+					<>
+						<p>{q.questionText}</p>
+						<div width="100%" height="100%">
+							<BarChart
+								width={500}
+								height={300}
+								data={dataForMultiple}
+								margin={{
+									top: 5,
+									right: 30,
+									left: 20,
+									bottom: 5,
+								}}
+							>
+								<CartesianGrid strokeDasharray="3 3" />
+								<XAxis dataKey="name" />
+								<YAxis />
+								<Tooltip />
+								<Legend />
+								<Bar dataKey={q.multipleChoice[0]} fill="#8884d8" />
+								<Bar dataKey={q.multipleChoice[1]} fill="#82ca9d" />
+								<Bar dataKey={q.multipleChoice[2]} fill="skyblue" />
+							</BarChart>
+						</div>
+					</>
+				))}
 			</div>
 
 			<div className="line">
-				<div width="100%" height="100%">
-					<p>How old are you?</p>
-					<LineChart
-						width={500}
-						height={300}
-						data={dataForNumber}
-						margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-					>
-						<CartesianGrid strokeDasharray="3 3" />
-						<XAxis dataKey="name" />
-						<YAxis />
-						<Tooltip />
-						<Legend />
-						<Line
-							type="monotone"
-							dataKey="response"
-							stroke="#8884d8"
-							activeDot={{ r: 8 }}
-						/>
-						
-					</LineChart>
-				</div>
+				{dataForNumber.map((q)=>(
+					<>
+						<div width="100%" height="100%">
+							<p>{q.questionText}</p>
+							<LineChart
+								width={500}
+								height={300}
+								data={dataForNumber}
+								margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+							>
+								<CartesianGrid strokeDasharray="3 3" />
+								<XAxis dataKey="name" />
+								<YAxis />
+								<Tooltip />
+								<Legend />
+								<Line
+									type="monotone"
+									dataKey="response"
+									stroke="#8884d8"
+									activeDot={{ r: 8 }}
+								/>
+								
+							</LineChart>
+						</div>
+					</>
+				))}
 			</div>
 
-			<div className="pie">
-				<div width="100%" height="100%">
-					<p>Did you do your assignments?</p>
-					<PieChart width={400} height={400}>
-						<Pie
-							data={dataForBool}
-							cx="50%"
-							cy="50%"
-							labelLine={false}
-							outerRadius={80}
-							fill="#8884d8"
-							dataKey="value"
-						>
-							{dataForBool.map((entry, index) => (
-							<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-							))}
-						</Pie>
-						<Legend/>
-					</PieChart>
-				</div>
+				<div className="pie">
+					{dataForBool.map((q)=>(
+						<>
+						<div width="100%" height="100%">
+							<p>{q.questionText}</p>
+							<PieChart width={400} height={400}>
+								<Pie
+									data={dataForBool}
+									cx="50%"
+									cy="50%"
+									labelLine={false}
+									outerRadius={80}
+									fill="#8884d8"
+									dataKey="value"
+								>
+									{dataForBool.map((entry, index) => (
+									<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+									))}
+								</Pie>
+								<Legend/>
+							</PieChart>
+						</div>
+						</>
+					))}
 			</div>
 		</div>
 	);
